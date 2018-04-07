d532febe5ffc095cc09359c3261adaec1b57cbf4
sergiocastri <scastrillon347@gmail.com>
2018-04-07 02:02:47 -0500

    initial heroku

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express  = require('express');
 const app = express();
 const path  = require('path');
 
-app.use(express.static(_dirname + '/src'));
+app.use(express.static(_dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
 app.get('/*', function(req, res){
-    res.sendfile(path.join(_dirname + '/src/index.html'));
+    res.sendfile(path.join(_dirname + '/dist/index.html'));
 })
 console.log('escuchando');
